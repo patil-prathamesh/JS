26abe0bc51b5227382a0ea20029ce343b59826f4
patil-prathamesh <69043646+patil-prathamesh@users.noreply.github.com>
2023-09-24 12:58:27 +0000

    modified

---
+++ variables.js
@@ -0,0 +1,11 @@
+const accountId = 123;
+let name = 'prthmsh'
+let roll = 132
+let state;
+console.table([accountId,name,roll,state])
+
+//conversion
+//Number() Boolean() String()
+let nem = ""
+let num = Boolean(nem) //false
+console.log(num);
